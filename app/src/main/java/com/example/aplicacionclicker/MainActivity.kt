4147edc8ad69b9d189bdcd41071b74582680f808
Marcos Contreras <marcos.contreras@alumno.iesluisvives.org>
2025-09-21 12:42:47 +0200

    primer commit con la primera version

---
+++ app/src/main/java/com/example/aplicacionclicker/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.aplicacionclicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aplicacionclicker.ui.theme.AplicacionClickerTheme
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+
+const val KILLS_PARA_NIVEL = 3
+const val KILLS_PARA_BOSS  = 15
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AplicacionClickerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    EnemigoClicker(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun EnemigoClicker(modifier: Modifier = Modifier) {
+    var nivel by remember { mutableStateOf(1) }
+    var danio by remember { mutableStateOf(1) }
+    var kills by remember { mutableStateOf(0) }
+    var killsTotales by remember { mutableStateOf(0) }
+    var esBoss by remember { mutableStateOf(false) }
+    var vidaEnemigoMaxima by remember { mutableStateOf((5..20).random()) }
+    var vidaEnemigoActual by remember { mutableStateOf(vidaEnemigoMaxima) }
+    var enemigoImagen by remember { mutableStateOf(R.drawable.enemigo1) }
+
+  var mensaje by remember { mutableStateOf<String?>(null) }
+
+    val normalEnemigo = listOf(
+        R.drawable.enemigo1,
+        R.drawable.enemigo2,
+        R.drawable.enemigo3
+    )
+    val bossEnemigo = listOf(
+        R.drawable.boss1,
+        R.drawable.boss2
+    )
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    )  {
+
+        Text(text = "Nivel: $nivel  •  Daño: $danio  •  Normales: $kills  •  Totales: $killsTotales")
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Image(
+            painter = painterResource(id = enemigoImagen),
+            contentDescription = "Enemigo",
+            modifier = Modifier.size(260.dp)
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "Vida: $vidaEnemigoActual/$vidaEnemigoMaxima")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                vidaEnemigoActual -= danio
+                if (vidaEnemigoActual <= 0) {
+                    killsTotales++
+                    if(esBoss) {
+                        nivel +=3
+                        danio+=3
+                        mensaje = "Has derrotado a un boss!"
+                        esBoss=false
+                        vidaEnemigoMaxima=(5..20).random()
+                        vidaEnemigoActual=vidaEnemigoMaxima
+                        enemigoImagen=normalEnemigo.random()
+                        kills++
+                    }else{
+                        kills++
+                        mensaje = "Has derrotado a un enemigo!"
+                        if (kills % KILLS_PARA_NIVEL==0) {
+                            nivel++
+                            danio++
+                            mensaje = "Has subido de nivel!"
+                        }
+                        if (kills >= KILLS_PARA_BOSS) {
+                            esBoss = true
+                            mensaje = "Ha aparecido un boss!"
+                            vidaEnemigoMaxima=(50..100).random()
+                            vidaEnemigoActual=vidaEnemigoMaxima
+                            enemigoImagen=bossEnemigo.random()
+                            kills=0
+
+                        }else{
+                            vidaEnemigoMaxima=(5..20).random()
+                            vidaEnemigoActual=vidaEnemigoMaxima
+                            enemigoImagen=normalEnemigo.random()
+
+                        }
+                    }
+                }
+            }
+        ) {
+        Text("Atacar")
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+
+        mensaje?.let { Text(it) }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun EnemyClickerPreview() {
+    EnemigoClicker()
+}
